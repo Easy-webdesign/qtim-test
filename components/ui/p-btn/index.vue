75fedd41fd8d543b76e30366b7bb46a56c79d509
Easy <easy-webdesign@yandex.ru>
2023-12-04 17:42:24 +0300

    Second commit

--- components/ui/p-btn/index.vue
+++ components/ui/p-btn/index.vue
@@ -1,6 +1,9 @@
 <template>
     <!-- Кнопка пагинации -->
-    <div :class="['v-qtim__btn-p', {'v-qtim__btn-p--active': active}]">
+    <div :class="['v-qtim__btn-p', {'v-qtim__btn-p--active': active}, type && `v-qtim__btn-p--${type}`]">
+        <svg v-if="type === 'next' || type === 'prev'"  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M9.5 7.5L14.5 12.5L9.5 17.5" stroke="#494949" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
         {{num}}
     </div>
 </template>
@@ -8,6 +11,7 @@
 <script lang='ts' setup>
     const props = defineProps({
         num: Number,
+        type: String,
         active: Boolean
     })
 </script>
